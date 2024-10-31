02aa1f2618add5faf93b39dfb7616b5644ba9ca6
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2024-10-31 14:17:21 -0600

    added batch script for param sweep cost reruns for atb 2030

---
+++ input/batch-scripts/paramsweep_cost_reruns/psrerun_rerun_ATB2030.sh
@@ -0,0 +1,15 @@
+#!/bin/bash                                                                                                             
+#SBATCH --job-name=rerun-psrerun_rerun_ATB2030
+#SBATCH --output=R-%x.%j.out
+#SBATCH --partition=standard
+#SBATCH --nodes=8
+#SBATCH --ntasks-per-node=100
+#SBATCH --time=2:00:00
+#SBATCH --account=hopp
+#SBATCH --mail-user egrant@nrel.gov
+#SBATCH --mail-type BEGIN,END,FAIL
+module load conda
+conda activate /scratch/egrant/ned_tools
+module load cray-mpich
+export TMPDIR=/scratch/egrant/sc_tmp/
+srun -N 8 --ntasks-per-node=100 /scratch/egrant/ned_tools/bin/python /scratch/egrant/NED-toolbox/toolbox/finance_reruns/rerun_simplex_new_costs_mpi.py rerun_ATB2030.yaml
